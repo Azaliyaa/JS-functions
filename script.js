fa4a73a097173ca6e02ac228360338cc53a9c07c
Azaliia Kasimova <azaliia_kasimova@epam.com>
2020-03-26 01:26:00 +0300

    add script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function compareTriplets(a, b) {
+    let aliceScore = 0;
+    let bobScore = 0;
+    for (let i=0; i<3; i++) {
+        if (a[i] < b[i]) {
+            bobScore++;
+        } else if (a[i] > b[i]) {
+            aliceScore++;
+        }
+    }
+    return [aliceScore, bobScore];
+}
 
+let alice = [5, 6, 7];
+let bob = [3, 6, 10];
+
+console.log(compareTriplets(alice, bob));
