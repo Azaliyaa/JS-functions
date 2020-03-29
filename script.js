0561a469158e850124ba29182e258cbb15494d51
Azaliia Kasimova <azaliia_kasimova@epam.com>
2020-03-29 21:41:49 +0300

    add timeInWords function

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function compareTriplets(a, b) {
 let alice = [5, 6, 7];
 let bob = [3, 6, 10];
 
-console.log(compareTriplets(alice, bob));
+// console.log(compareTriplets(alice, bob));
 
 function getSecondLargest(arr) {
     let max = arr.reduce((biggest, current) => {
@@ -29,7 +29,7 @@ function getSecondLargest(arr) {
 }
 let arr = [5, 6, 3, 2, 7];
 
-console.log(getSecondLargest(arr));
+// console.log(getSecondLargest(arr));
 
 function climbingLeaderboard(scores, alice) {
     let ranks = [];
@@ -49,4 +49,36 @@ function climbingLeaderboard(scores, alice) {
 let scores = [100, 90, 80, 75, 60];
 let aliceScore = [50, 65, 77, 90, 102];
 
-console.log(climbingLeaderboard(scores, aliceScore));
+// console.log(climbingLeaderboard(scores, aliceScore));
+
+function timeInWords(h, m) {
+    let timeString = '';
+    let minutes = 'minutes';
+    let time = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
+     "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
+     "twenty one", "twenty two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven",
+     "twenty eight", "twenty nine"];
+     if (m  == 1 || m  == 59) {
+        minutes = 'minute';
+    }
+    if (m < 30 && m != 15 && m != 0) {
+        timeString = `${time[m]} ${minutes} past ${time[h]}`;
+    } else if (m > 30 && m !=45) {
+        timeString = `${time[60-m]} ${minutes} to ${time[h+1]}`;
+    } else if (m == 15) {
+        timeString = `quarter past ${time[h]}`;
+    } else if (m == 45) {
+        timeString = `quarter to ${time[h+1]}`;
+    } else if (m == 30) {
+        timeString = `half past ${time[h]}`;
+    } else {
+        timeString = `${time[h]} o' clock`;
+    }
+
+    return timeString;
+}
+
+h = 5;
+m = 56;
+
+console.log(timeInWords(h, m));
